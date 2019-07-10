1a89a5845ee97ded14a46b96287c46f04a286091
anthonypiazza <piazza.anthony.n@gmail.com>
2019-07-10 12:15:06 -0500

    created axios GET request, created unique NASA API key, pulled in URL of image to render successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './App.css';
+// import Photo from './components/Photo,js';
 
 function App() {
+// Initialize state to hold the image URL
+  const [apod, setApod] = useState();
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=k89Bsucu73bZRwuwA6f9nbbP7sYsyJPKahIhzpL5&date=2012-03-14')
+      .then(res => {
+        console.log(res)
+        setApod(res.data.url)
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Photo of the Day</h1>
+      <img src={apod} alt="Space" />
     </div>
   );
 }

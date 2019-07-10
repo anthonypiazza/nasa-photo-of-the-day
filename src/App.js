bec9ab55f7ea707b3069a0ee4aca42bd928797ee
Anthony Piazza <49876466+anthonypiazza@users.noreply.github.com>
2019-07-10 16:31:50 -0500

    Revert "Day 1 MVP: GET request with Axios"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-import './App.css';
-import Photo from './components/Photo.js';
+import React from "react";
+import "./App.css";
 
 function App() {
-// Initialize state to hold the image URL
-  const [apod, setApod] = useState();
-  useEffect(() => {
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=k89Bsucu73bZRwuwA6f9nbbP7sYsyJPKahIhzpL5&date=2012-03-14')
-      .then(res => {
-        console.log(res.data.url)
-        setApod(res.data.url)
-      })
-      .catch(err => console.log(err));
-  }, []);
-  console.log(apod)
   return (
     <div className="App">
-      <h1>NASA Photo of the Day</h1>
-      <Photo apod={apod} />
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
     </div>
   );
 }
